0a1b352a20178f55efb563889e01a3f0b8f453d4
Missguiding <lindsey141080@hotmail.com>
2024-07-23 19:47:31 +0200

    Add project files.

---
+++ HeerlijkeHerinneringen.Data/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace HeerlijkeHerinneringen.Data
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ HeerlijkeHerinneringen/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HeerlijkeHerinneringen.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
