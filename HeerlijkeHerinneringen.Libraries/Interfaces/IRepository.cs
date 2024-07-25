bd0fb96495468179d0488abef917adf8b612427c
Missguiding <lindsey141080@hotmail.com>
2024-07-25 15:09:48 +0200

    Aanmaak IRepository

---
+++ HeerlijkeHerinneringen.Libraries/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeerlijkeHerinneringen.Libraries.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        public T GetById(int id);
+        public void Add(T entity);
+        public void Update(T entity);
+        public void Delete(int id);
+    }
+}
