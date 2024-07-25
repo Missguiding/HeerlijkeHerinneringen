782c292cc8a2899453992d048c0f4ae9ed492df6
Missguiding <lindsey141080@hotmail.com>
2024-07-25 19:38:39 +0200

    Toevoeging AutoMapper

---
+++ HeerlijkeHerinneringen.Libraries/Profiles/AutoMapperProfiles.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using HeerlijkeHerinneringen.Data.Models;
+using HeerlijkeHerinneringen.Libraries.ViewModels;
+
+namespace HeerlijkeHerinneringen.Libraries.Profiles
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles() { }
+    }
+}
