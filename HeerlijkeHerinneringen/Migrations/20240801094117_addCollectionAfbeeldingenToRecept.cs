643ba32ed78ff516ba3964afb4c6fd5a2efb2422
Missguiding <lindsey141080@hotmail.com>
2024-09-20 10:27:21 +0200

    Model - Wijziging byte afbeeldig naar ICollection + Migration

---
+++ HeerlijkeHerinneringen/Migrations/20240801094117_addCollectionAfbeeldingenToRecept.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HeerlijkeHerinneringen.Migrations
+{
+    /// <inheritdoc />
+    public partial class addCollectionAfbeeldingenToRecept : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Afbeelding",
+                table: "Recepts");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte>(
+                name: "Afbeelding",
+                table: "Recepts",
+                type: "tinyint",
+                nullable: true);
+        }
+    }
+}
