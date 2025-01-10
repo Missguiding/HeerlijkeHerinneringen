51c5f519d8872ee2f188b7b61bd8266de00fe9dc
Missguiding <lindsey141080@hotmail.com>
2025-01-10 11:46:35 +0100

    Creatie: BenodigdheidController

---
+++ HeerlijkeHerinneringen/Controllers/BenodigdheidController.cs
@@ -0,0 +1,91 @@
+﻿using HeerlijkeHerinneringen.Data.Models;
+using HeerlijkeHerinneringen.Libraries.Services;
+using HeerlijkeHerinneringen.Libraries.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HeerlijkeHerinneringen.Controllers
+{
+    public class BenodigdheidController : Controller
+    {
+        public BenodigdheidService _benodigdheidService;
+
+        public BenodigdheidController(BenodigdheidService benodigdheidService)
+        {
+            _benodigdheidService = benodigdheidService;
+        }
+        // GET: BenodigdheidController
+        public ActionResult Index()
+        {
+            return View(_benodigdheidService.GetAll());
+        }
+
+        // GET: BenodigdheidController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(_benodigdheidService.GetById(id));
+        }
+
+
+        // GET: BenodigdheidController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BenodigdheidController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(BenodigdheidViewModel benodigdheid)
+        {
+            // Voeg de nieuwe benodigheid toe aan de service/data
+            _benodigdheidService.Add(benodigdheid);
+           
+            return RedirectToAction("Index");
+        }
+
+        // GET: BenodigdheidController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View(_benodigdheidService.GetById(id));
+        }
+
+        // POST: BenodigdheidController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, BenodigdheidViewModel benodigdheid)
+        {
+            try
+            {
+                _benodigdheidService.Update(benodigdheid);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BenodigdheidController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View(_benodigdheidService.GetById(id));
+        }
+
+        // POST: BenodigdheidController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, BenodigdheidViewModel benodigdheid)
+        {
+            try
+            {
+                _benodigdheidService.Delete(benodigdheid.Id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
